a3d864120a69d1f579f8d8f82361625bb53484d6
Naomi Martinez Wilson <78883410+naomiim@users.noreply.github.com>
2024-11-11 18:13:00 -0600

    Update models.py
    database

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    student_id = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.student_id})"
+
+class Course(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    subject = models.CharField(max_length=100)
+    course_number = models.IntegerField()
+    num_of_credits = models.IntegerField()
+    professor = models.CharField(max_length=100)
+    seats_remaining = models.IntegerField()
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    students = models.ManyToManyField(Student, related_name="courses", blank=True)
+
+    def __str__(self):
+        return f"{self.subject} {self.course_number} - {self.professor}"
